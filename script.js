89b642a9b5f2865c82484e1bfa4ec3e4416ba8b8
f0jzd <tomassavela@gmail.com>
2026-06-10 14:49:26 +0200

    Added a ton of semantics, or atleast attempted to

--- script.js
+++ script.js
@@ -10,12 +10,23 @@ const backdrop = document.getElementById("backdrop");
 document.getElementById("openMenu").addEventListener("click", () => {
   sidebar.classList.add("open");
   backdrop.classList.add("open");
+
+  sidebar.inert = false;
 });
 
 function closeMenu() {
   sidebar.classList.remove("open");
   backdrop.classList.remove("open");
+
+  sidebar.inert = true;
 }
 
 document.getElementById("closeMenu").addEventListener("click", closeMenu);
 backdrop.addEventListener("click", closeMenu);
+
+//ESC key handler
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeMenu();
+  }
+});
