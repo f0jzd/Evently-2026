c08ba14459357b52859bd89c0a81c412e3279182
f0jzd <tomassavela@gmail.com>
2026-06-10 09:20:08 +0200

    Added hover functionality
    Added hover functionality to almost everything
    Added sidebar with burger menu to open on lower screens

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const icon = document.getElementById("icon");
 icon.addEventListener("click", () => {
   icon.classList.toggle("active");
 });
+
+const sidebar = document.getElementById("sidebar");
+const backdrop = document.getElementById("backdrop");
+
+document.getElementById("openMenu").addEventListener("click", () => {
+  sidebar.classList.add("open");
+  backdrop.classList.add("open");
+});
+
+function closeMenu() {
+  sidebar.classList.remove("open");
+  backdrop.classList.remove("open");
+}
+
+document.getElementById("closeMenu").addEventListener("click", closeMenu);
+backdrop.addEventListener("click", closeMenu);
